009464defa043d2eb475ecc87bef06be6b35a770
master <e>
2019-12-13 16:35:47 +0100

    dsa

--- SzamelyzetApp/Program.cs
+++ SzamelyzetApp/Program.cs
@@ -10,7 +10,22 @@ namespace SzamelyzetApp
     {
         static void Main(string[] args)
         {
+            var boss = new Szemely("Majer Zoltán",new DateTime(1980,10,1),"boss");
+            var sz1 = new Szemely("Gyorgyi Tamás",new DateTime(1979,8,12),"doktor");
+            var sz2 = new Szemely("Czinkóci Tamás",new DateTime(1973,1,25),"webmester");
+            var sz3 = new Szemely("Ben Dover",new DateTime(1982,3,2),"analitikus");
+            var sz4 = new Szemely("Engler Ervin",new DateTime(1972,6,8),"hacker");
+            Szemelyzet szemelyzet = new Szemelyzet(boss);
 
+            boss.Hozzaad(sz1);
+            boss.Hozzaad(sz2);
+            sz1.Hozzaad(sz3);
+            sz1.Hozzaad(sz4);
+
+            Szemely keresett = szemelyzet.Keres("Ben Dover", new DateTime(1982, 3, 2));
+            Console.WriteLine(keresett);
+
+            Console.ReadLine();
         }
     }
 }
---
+++ SzamelyzetApp/Szemely.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzamelyzetApp
+{
+    class Szemely : IComparable<Szemely>
+    {
+        readonly string nev;
+        readonly DateTime szuletes;
+        string beosztas;
+
+        ISet<Szemely> beosztottak = new SortedSet<Szemely>();
+
+        public Szemely(string nev, DateTime szuletes, string beosztas)
+        {
+            this.nev = nev;
+            this.szuletes = szuletes;
+            this.beosztas = beosztas;
+        }
+        internal Szemely Keres(string nev, DateTime szuletes)
+        {
+            Szemely keresett = null;
+            if (this.nev.Equals(nev) && this.szuletes.Equals(szuletes))
+            {
+                return this;
+            }
+            foreach (var b in beosztottak)
+            {
+                if (b.nev.Equals(nev) && b.szuletes.Equals(szuletes))
+                {
+                    return b;
+                }
+                keresett = b.Keres(nev, szuletes);
+            }
+            return keresett;
+        }
+
+        public int CompareTo(Szemely other)
+        {
+            int i = this.nev.CompareTo(other.nev);
+            if (i != 0)
+            {
+                return i;
+            }
+            return this.szuletes.CompareTo(other.szuletes);
+        }
+
+        public void Hozzaad(Szemely beosztott)
+        {
+            this.beosztottak.Add(beosztott);
+        }
+
+        public override string ToString()
+        {
+            return nev + " " + szuletes + " (" + beosztas +")";
+        }
+    }
+}
